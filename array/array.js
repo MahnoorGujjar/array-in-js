82d99a5061698eb0dcc933671a85fb0615d5a58e
Mahnoor Gujjar <160156566+MahnoorGujjar@users.noreply.github.com>
2024-10-18 16:47:46 +0500

    Add files via upload

---
+++ array/array.js
@@ -0,0 +1,158 @@
+// ARRAY 
+
+// DECLARING AN ARRAY
+
+// let myArr = []
+
+                //    /**************/////////////////////////////////**********************/ */ */
+
+
+//INITIALAZING AN ARRAY
+
+// let myArr = [1 , 2 , 3 , 4 , 5]
+// console.log(myArr);
+
+
+//////////////// OUTPUT WILL BE (1 2 3 4 5 ) ///////////////////
+
+
+
+                //    /**************/////////////////////////////////**********************/ 
+
+
+//PRINTING MULTIPLE VALUES FROM ARRAY
+
+// let studentInfo = ["mahnoor", "muhammad suleman" , 123456 , "female" , 25]
+// console.log(studentInfo[0], studentInfo[2]);
+
+
+//////////////// OUTPUT WILL BE (MAHNOOR 123456) ///////////////////
+
+
+
+                //    /**************/////////////////////////////////**********************/ */ */
+
+//WE CAM ALSO CREATE AN ARRAY , AND THEN PROVIDE AN ALEMENT
+
+// let cars = []
+
+// cars [0] =  "BMW"
+// cars [1] = "CIVIC"
+// cars [2] = "AUDI"
+
+// console.log(cars);
+//////////////// OUTPUT WILL BE (BMW CIVIC AUDI) ///////////////////
+
+
+
+// console.log(cars[2]);
+//////////////// OUTPUT WILL BE (AUDI) ///////////////////
+
+                     ////////////**************************//////////////////***************************/////////////////////
+
+
+
+ /////////////////////ARRAY'S PRPEETIES///////////////////////////
+
+// LENGTH : RETURNS THE LENGTH OF THE ARRAY
+
+// let myArr3 = [1,2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(myArr3.length);
+
+//////////////// OUTPUT WILL BE (10) ///////////////////
+
+
+
+                     ////////////**************************//////////////////***************************/////////////////////
+
+// let num = [1,2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// document.write(num.length);
+
+/////////////// OUTPUT WILL BE (10) ///////////////////
+
+
+
+                     ////////////**************************//////////////////***************************/////////////////////
+
+
+// PUSH() : ADDS ONE OR MORE ELEMENTS TO THE END OF THE ARRAY
+
+
+// let myArr3 = [1,2, 3, 4, 5];
+
+// console.log(myArr3.push("MAHNOOR"));
+
+// console.log(myArr3);
+
+//////////////// OUTPUT WILL BE (1,2, 3, 4, 5 ,MAHNOOR ) ///////////////////
+
+                     ////////////**************************//////////////////***************************/////////////////////
+
+// let studentInfo = ["mahnoor", "muhammad suleman" , 123456 , "female" , 25]
+// console.log(studentInfo.push("web and app development"))
+// console.log(studentInfo);
+
+
+/////////////// OUTPUT WILL BE (6) ///////////////////
+
+
+/////////////// OUTPUT WILL BE (['mahnoor', 'muhammad suleman', 123456, 'female', 25, 'web and app development') ///////////////////
+
+
+
+
+                     ////////////**************************//////////////////***************************/////////////////////
+
+
+
+//POP :REMOVES THE LAST ELEMENT OF THE ARRAY
+
+// let myWorld = ["noor" , 25 , "jungkook" , 26 , "bangtan" , "army" , 'hybe' ]
+
+// console.log(myWorld.pop());
+// console.log(myWorld);
+
+
+/////////////// OUTPUT WILL BE (hybe) ///////////////////
+
+/////////////// OUTPUT WILL BE ((6) ['noor', 25, 'jungkook', 26, 'bangtan', 'army']) ///////////////////
+
+
+
+
+                   //////***********************************//////////////////***************************///////////////////////////
+
+
+
+//SHIFT : REMOVES THE FIRST ELEMENT FROM THE ARRAY
+
+// let numbers = [1 , 2, 3 ,4 ,5 ]
+
+// console.log(numbers.shift());
+
+/////////////// OUTPUT WILL BE (1) ///////////////////
+
+// console.log(numbers);
+
+/////////////// OUTPUT WILL BE (2 ,3 ,4 ,5) ///////////////////
+
+
+
+                        ///////***************************************//////////////////***************************/////////////////
+
+
+// UNSHIFT : ADDS ONE OR MORE ELEMNT TO THE BEGINNING OF THE ARRAY
+
+// let details = [ "sheri" , "bazaid" , "adnan"]
+
+// console.log(details.unshift("rumaisa"));
+/////////////// OUTPUT WILL BE (4) ///////////////////
+
+
+// console.log(details);
+/////////////// OUTPUT WILL BE (['rumaisa', 'sheri', 'bazaid', 'adnan') ///////////////////
+
+
+                     ////////////***************************************//////////////////***************************/////////////////
